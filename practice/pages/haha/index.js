8516347a7eaa59e9bb66214c93986a0246729006
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-11-02 22:24:36 +0900

    getStateProps로 api 받기

--- practice/pages/haha/index.js
+++ practice/pages/haha/index.js
@@ -1,7 +1,16 @@
 import React from "react";
 import Head from "next/head";
+import axios from "axios";
+import styles from "../../styles/Hahas.module.css";
 
-export default function HahaIndex() {
+export const getStaticProps = async () => {
+  const res = await axios.get("https://jsonplaceholder.typicode.com/users");
+  return {
+    props: { data: res.data },
+  };
+};
+
+export default function HahaIndex({ data }) {
   return (
     <>
       <Head>
@@ -9,7 +18,14 @@ export default function HahaIndex() {
         <meta name="keywords" content="Teemo"></meta>
       </Head>
       <div>
-        <h1>HaHa Index</h1>
+        <h1>All HaHa</h1>
+        {data.map((i) => (
+          <div key={i.id}>
+            <a className={styles.single}>
+              <h3>{i.name}</h3>
+            </a>
+          </div>
+        ))}
       </div>
     </>
   );
