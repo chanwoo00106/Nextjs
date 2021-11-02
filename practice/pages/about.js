dc5527e60d7adf3d20fafb361fc9f08427257ba2
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-11-02 19:49:30 +0900

    routes 배우기

---
+++ practice/pages/about.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum
+        doloribus, perspiciatis labore eos laudantium hic dignissimos iste cum
+        obcaecati quibusdam cupiditate suscipit at et mollitia, iure magni.
+        Corporis, illo autem!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum
+        doloribus, perspiciatis labore eos laudantium hic dignissimos iste cum
+        obcaecati quibusdam cupiditate suscipit at et mollitia, iure magni.
+        Corporis, illo autem!
+      </p>
+    </div>
+  );
+}
---
+++ practice/pages/haha/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function HahaIndex() {
+  return (
+    <div>
+      <h1>HaHa Index</h1>
+    </div>
+  );
+}
