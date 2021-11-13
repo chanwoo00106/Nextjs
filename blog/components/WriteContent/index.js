155d2d38a196dcb767c541b1975cbfca72d90a98
변찬우 <chan00106@naver.com>
2021-11-13 12:54:04 +0900

    대충 바꾸기

--- blog/components/WriteContent/index.js
+++ blog/components/WriteContent/index.js
@@ -4,6 +4,11 @@ const WriteContent = () => {
   return (
     <S.Wrapper>
       <input type="text" placeholder="제목 입력" className="title" />
+      <label className="choiceFile" htmlFor="file">
+        사진 선택
+      </label>
+      <input type="file" id="file" />
+      <textarea placeholder="글을 작성해 주세요"></textarea>
       <button>Submit</button>
     </S.Wrapper>
   );
--- blog/components/WriteContent/style.js
+++ blog/components/WriteContent/style.js
@@ -11,13 +11,47 @@ export const Wrapper = styled.div`
   .title {
     width: 100%;
     height: 40px;
-    padding: 0 10px;
+    padding: 10px;
     font-size: 1.5rem;
-    margin-bottom: 30px;
     outline: none;
     border: none;
     background: #efefef;
     border-radius: 10px;
+    margin-bottom: 30px;
+  }
+
+  .choiceFile {
+    padding: 10px 20px;
+    background: #efefef;
+    border-radius: 10px;
+    cursor: pointer;
+    transition: 0.2s;
+
+    &:hover {
+      background: #c5c5c5;
+    }
+  }
+
+  input[type="file"] {
+    position: absolute;
+    width: 0;
+    height: 0;
+    padding: 0;
+    overflow: hidden;
+    border: 0;
+  }
+
+  textarea {
+    margin: 30px 0;
+    width: 100%;
+    height: 400px;
+    outline: none;
+    border: none;
+    border-radius: 10px;
+    background: #efefef;
+    padding: 20px;
+    font-size: 1rem;
+    resize: none;
   }
 
   button {
