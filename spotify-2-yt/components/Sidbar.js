e4d61b4c11860716a90f8bab72d946e12c9be51a
변찬우 <chan00106@naver.com>
2021-12-11 23:20:24 +0900

    대충 테스트

---
+++ spotify-2-yt/components/Sidbar.js
@@ -0,0 +1,15 @@
+import { HomeIcon } from "@heroicons/react/outline";
+
+function SideBar() {
+  return (
+    <div>
+      <div>
+        <button>
+          <HomeIcon className="h-5 w-5" />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default SideBar;
