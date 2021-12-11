030e7ba0d51216731ab444cf8194a259065d526b
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-12-11 20:51:38 +0900

    recoil 삭제

--- todo-list/pages/_app.js
+++ todo-list/pages/_app.js
@@ -1,12 +1,7 @@
 import "../styles/globals.css";
-import { RecoilRoot } from "recoil";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <RecoilRoot>
-      <Component {...pageProps} />
-    </RecoilRoot>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
