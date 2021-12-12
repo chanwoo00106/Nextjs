6e9108c8f4ef117bd330e1a8bfd190729330e8c9
변찬우 <chan00106@naver.com>
2021-12-12 17:51:39 +0900

    github login

---
+++ next-auth-tutorial/pages/api/auth/[...nextauth].js
@@ -0,0 +1,16 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    // ...add more providers here
+  ],
+
+  // A database is optional, but required to persist accounts in a database
+  database: process.env.DATABASE_URL,
+});
