8b8e04e6ef8e1831ada5cbe669cbf599323075b3
변찬우 <chan00106@naver.com>
2021-12-12 15:41:28 +0900

    원하는 강의가 아니었음..

--- spotify-2-yt/pages/api/auth/[...nextauth].js
+++ spotify-2-yt/pages/api/auth/[...nextauth].js
@@ -1,6 +1,28 @@
 import NextAuth from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
-import { LOGIN_URL } from "../../../lib/spotify";
+import spotifyApi, { LOGIN_URL } from "../../../lib/spotify";
+
+async function refreshAccessToken(token) {
+  try {
+    spotifyApi.setAccessToken(token.accessToken);
+    spotifyApi.setRefreshToken(token.refreshToken);
+    const { body: refreshedToken } = await spotifyApi.refreshAccessToken();
+    console.log("refreshed token");
+
+    return {
+      ...token,
+      accessToken: refreshedToken.access_token,
+      accessTokenExpires: Date.now + refreshedToken.expires_in * 1000,
+      refreshToken: refreshedToken.refresh_token ?? token.refreshToken,
+    };
+  } catch (e) {
+    console.log(e);
+    return {
+      ...token,
+      error: "RefreshAccessTokenError",
+    };
+  }
+}
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -12,4 +34,34 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  secret: process.env.JWT_SECRET,
+  pages: {
+    signIn: "/login",
+  },
+  callbacks: {
+    async jwt({ token, account, user }) {
+      if (account && user) {
+        return {
+          ...token,
+          accessToken: account.access_token,
+          refreshToken: account.refresh_token,
+          username: account.providerAccountId,
+          accessTokenExpires: account.expires_at * 1000,
+        };
+      }
+      if (Date.now() < token.accessTokenExpires) {
+        return token;
+      }
+
+      console.log("ACCESS TOKEN HAS EXPIRED, REFRESHING...");
+      return await refreshAccessToken(token);
+    },
+    async session({ session, token }) {
+      session.user.accessToken = token.accessToken;
+      session.user.refreshToken = token.refreshToken;
+      session.user.username = token.username;
+
+      return session;
+    },
+  },
 });
--- spotify-2-yt/pages/login.js
+++ spotify-2-yt/pages/login.js
@@ -1,9 +1,30 @@
-function Login() {
+import { getProviders, signIn } from "next-auth/react";
+
+function Login({ providers }) {
   return (
-    <div>
-      <h1>this is a login page</h1>
+    <div className="flex flex-col items-center bg-black min-h-screen w-full justify-center">
+      <img className="w-52 mb-5" src="https://links.papareact.com/9xl" />
+      {Object.values(providers).map((provider) => (
+        <div key={provider.name}>
+          <button
+            onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+            className="bg-[#18d860] text-white p-5 rounded-full"
+          >
+            Login with {provider.name}
+          </button>
+        </div>
+      ))}
     </div>
   );
 }
 
 export default Login;
+
+export async function getServerSideProps() {
+  const providers = await getProviders();
+  return {
+    props: {
+      providers,
+    },
+  };
+}
