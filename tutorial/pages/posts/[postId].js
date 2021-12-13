91a73ae791694f5f899ecb08c76867aa574fd304
변찬우 <chan00106@naver.com>
2021-12-13 23:24:07 +0900

    learn getStaticPaths

---
+++ tutorial/pages/posts/[postId].js
@@ -0,0 +1,46 @@
+import axios from "axios";
+import { useRouter } from "next/router";
+
+export default function Post({ post }) {
+  const router = useRouter();
+  const onClick = () => router.back();
+
+  if (router.isFallback) {
+    return <h1>Loading...</h1>;
+  }
+
+  return (
+    <>
+      <h2>
+        {post.id} {post.title}
+      </h2>
+      <p>{post.body}</p>
+      <button onClick={onClick}>back</button>
+    </>
+  );
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  const { data } = await axios.get(
+    `https://jsonplaceholder.typicode.com/posts/${params.postId}`
+  );
+  return {
+    props: {
+      post: data,
+    },
+  };
+}
+
+export async function getStaticPaths({ params }) {
+  const { data } = await axios.get(
+    `https://jsonplaceholder.typicode.com/posts`
+  );
+  const paths = data.slice(0, 3).map((post) => ({
+    params: { postId: post.id.toString() },
+  }));
+  return {
+    paths,
+    fallback: true,
+  };
+}
---
+++ tutorial/pages/posts/index.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import Link from "next/link";
+
+export default function PostList({ posts }) {
+  return (
+    <>
+      <h1>List of Posts</h1>
+      {posts.map((post) => (
+        <div key={post.id}>
+          <h2>
+            <Link href={`/posts/${post.id}`} passHref>
+              <a>
+                {post.id} {post.title}
+              </a>
+            </Link>
+          </h2>
+          <hr />
+        </div>
+      ))}
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  const { data } = await axios.get(
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+  return {
+    props: { posts: data.slice(0, 3) },
+  };
+}
