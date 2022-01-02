8b5810759f07e7c6b5482e08f3adb16e689395e9
변찬우 <chan00106@naver.com>
2022-01-02 18:51:51 +0900

    add style

--- nextjs-intro/components/NavBar.js
+++ nextjs-intro/components/NavBar.js
@@ -1,15 +1,16 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import style from "./NavBar.module.css";
 
 export default function NavBar() {
   const router = useRouter();
   return (
     <nav>
       <Link href="/">
-        <a style={{ color: router.pathname === "/" ? "red" : "blue" }}>Home</a>
+        <a className={router.pathname === "/" ? style.active : ""}>Home</a>
       </Link>
       <Link href="/about">
-        <a style={{ color: router.pathname === "/about" ? "red" : "blue" }}>
+        <a className={router.pathname === "/about" ? style.active : ""}>
           About
         </a>
       </Link>
