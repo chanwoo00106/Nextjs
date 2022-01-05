0623124c6a5c97059384d82bd3429ed7e5322a95
변찬우 <chan00106@naver.com>
2022-01-06 01:29:53 +0900

    styling and request api

--- nextjs-intro/pages/about.js
+++ nextjs-intro/pages/about.js
@@ -1,6 +1,9 @@
-export default function about() {
+import Seo from "../components/Seo";
+
+export default function About() {
   return (
     <div>
+      <Seo title="About" />
       <h1>hello</h1>
     </div>
   );
--- nextjs-intro/pages/index.js
+++ nextjs-intro/pages/index.js
@@ -1,12 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Seo from "../components/Seo";
+
+const API_KEY = "8e68f7c9518c4c4350c9d3452bd2af65";
 
 export default function Home() {
-  const [counter, setCounter] = useState(0);
+  const [movies, setMovies] = useState();
+
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+      console.log(results);
+      setMovies(results);
+    })();
+  }, []);
 
   return (
     <div>
-      <h1>{counter}</h1>
-      <button onClick={() => setCounter(counter + 1)}>+</button>
+      <Seo title="Main" />
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </div>
   );
 }
