9992cf23706382ab0932d53a2b2cc00529168d60
변찬우 <chan00106@naver.com>
2022-01-06 14:20:52 +0900

    movie detail

--- nextjs-intro/pages/index.js
+++ nextjs-intro/pages/index.js
@@ -1,22 +1,53 @@
 import Seo from "../components/Seo";
+import Link from "next/link";
+import axios from "axios";
+import { useRouter } from "next/router";
 
 export async function getServerSideProps() {
-  const { results } = await (
-    await fetch("http://localhost:3000/api/movies")
-  ).json();
+  const { data } = await axios("http://localhost:3000/api/movies");
   return {
-    props: { results },
+    props: {
+      results: data.results,
+    },
   };
 }
 
 export default function Home({ results }) {
+  const router = useRouter();
+  const onClick = (id, title) => {
+    router.push(
+      {
+        pathname: `movies/${id}`,
+        query: {
+          title: title,
+        },
+      },
+      `movies/${id}`
+    );
+  };
   return (
     <div className="container">
       <Seo title="Main" />
       {results?.map((movie) => (
-        <div key={movie.id} className="card">
+        <div
+          onClick={() => onClick(movie.id, movie.original_title)}
+          key={movie.id}
+          className="card"
+        >
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
-          <h4>{movie.original_title}</h4>
+          <Link
+            href={{
+              pathname: `movies/${movie.id}`,
+              query: {
+                title: movie.original_title,
+              },
+            }}
+            as={`movie/${movie.id}`}
+          >
+            <a>
+              <h4>{movie.original_title}</h4>
+            </a>
+          </Link>
         </div>
       ))}
       <style jsx>{`
