b775ddabbad830aa9ea4216a8e38cef5339d86f7
변찬우 <chan00106@naver.com>
2022-01-07 22:37:06 +0900

    이젠 진짜 모르겠다

--- spotify/pages/index.js
+++ spotify/pages/index.js
@@ -1,11 +1,11 @@
 import Head from "next/head";
+import Sidebar from "../components/Sidebar";
 
 export default function Home() {
   return (
-    <div>
-      <Head>
-        <title>Create Next App</title>
-      </Head>
+    <div className="overflow-hidden bg-black h-screen">
+      <h1>hello</h1>
+      <Sidebar />
     </div>
   );
 }
