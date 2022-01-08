56e83a501aa7ec59f362aced711ad17878f46eec
변찬우 <chan00106@naver.com>
2022-01-08 18:39:43 +0900

    Add prisma database

---
+++ database-prisma/prisma/migrations/20220108090748_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Contact" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "avatar" TEXT NOT NULL
+);
