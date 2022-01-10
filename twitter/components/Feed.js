caa7f3975ec81262f6d2880de6c228e532515a94
변찬우 <chan00106@naver.com>
2022-01-11 08:35:18 +0900

    add input

--- twitter/components/Feed.js
+++ twitter/components/Feed.js
@@ -1,4 +1,5 @@
 import { SparklesIcon } from "@heroicons/react/outline";
+import Input from "./Input";
 
 function Feed() {
   return (
@@ -9,6 +10,7 @@ function Feed() {
           <SparklesIcon className="h-5 text-white" />
         </div>
       </div>
+      <Input />
     </div>
   );
 }
---
+++ twitter/components/Input.js
@@ -0,0 +1,103 @@
+/* eslint-disable @next/next/no-img-element */
+import {
+  CalendarIcon,
+  ChartBarIcon,
+  EmojiHappyIcon,
+  PhotographIcon,
+  XIcon,
+} from "@heroicons/react/outline";
+import { useRef, useState } from "react";
+import { Picker } from "emoji-mart";
+import "emoji-mart/css/emoji-mart.css";
+
+function Input() {
+  const [input, setInput] = useState("");
+  const [selectFile, setSelectFile] = useState();
+  const filePickerRef = useRef(null);
+  const [showEmojis, setShowEmojis] = useState(false);
+
+  const addImageToPost = () => {};
+
+  const addEmoji = (e) => {
+    const sym = e.unified.split("-");
+    const codesArray = sym.map((i) => `0x${i}`);
+    const emoji = String.fromCodePoint(codesArray);
+    setInput(input + emoji);
+  };
+
+  return (
+    <div
+      className={`border-b border-gray-700 p-3 flex space-x-3  overflow-y-hidden`}
+    >
+      <img
+        src="https://yt3.ggpht.com/yti/APfAmoEcGZncTmkGdWJSEn3kU0JOqJg3xgGuGevK7tLW=s88-c-k-c0x00ffffff-no-rj-mo"
+        alt="profile"
+        className="h-10 w-10 rounded-full cursor-pointer"
+      />
+      <div className="w-full divide-y divide-gray-700">
+        <div className={``}>
+          <textarea
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            rows="2"
+            className="bg-transparent outline-none text-[#d9d9d9] text-lg placeholder-gray-500 tracking-wide w-full min-h-[50px] overflow-auto"
+            placeholder="What's happening?"
+          />
+
+          {selectFile && (
+            <div className="relative">
+              <div
+                onClick={() => setSelectFile(null)}
+                className="absolute w-8 h-8 bg-[#15181c] hover:bg-[#272c26] bg-opacity-75 rounded-full flex items-center justify-center top-1 left-1 cursor-pointer"
+              >
+                <XIcon className="text-white h-5" />
+              </div>
+              <img
+                src={selectFile}
+                alt=""
+                className="rounded-2xl max-h-80 object-contain"
+              />
+            </div>
+          )}
+        </div>
+        <div className="flex items-center justify-between pt-2.5">
+          <div className="flex items-center">
+            <div className="icon" onClick={() => filePickerRef.current.click()}>
+              <PhotographIcon className="h-[22px] text-[#1d9bf0]" />
+              <input
+                type="file"
+                onChange={addImageToPost}
+                ref={filePickerRef}
+                hidden
+              />
+            </div>
+            <div className="icon rotate-90">
+              <ChartBarIcon className="h-[22px] text-[#1d9bf0]" />
+            </div>
+            <div className="icon" onClick={() => setShowEmojis(!showEmojis)}>
+              <EmojiHappyIcon className="h-[22px] text-[#1d9bf0]" />
+            </div>
+            <div className="icon">
+              <CalendarIcon className="h-[22px] text-[#1d9bf0]" />
+            </div>
+            {showEmojis && (
+              <Picker
+                onSelect={addEmoji}
+                style={{
+                  position: "absolute",
+                  marginTop: "465px",
+                  marginLeft: -40,
+                  maxWidth: "320px",
+                  borderRadius: "20px",
+                }}
+                theme="dark"
+              />
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Input;
