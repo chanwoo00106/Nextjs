a56ad77ea8434ba33d5d5ba82abdc5570e20e21f
변찬우 <chan00106@naver.com>
2022-01-10 13:03:17 +0900

    Add Feed top

--- twitter/pages/index.js
+++ twitter/pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Feed from "../components/Feed";
 import Sidebar from "../components/Sidebar";
 
 export default function Home() {
@@ -12,6 +13,7 @@ export default function Home() {
 
       <main className="min-h-screen flex max-w-[1500px] mx-auto">
         <Sidebar />
+        <Feed />
       </main>
     </div>
   );
