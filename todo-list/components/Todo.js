4c843b02d962efeef8c10fae3cc1f82c754df631
변찬우 <chan00106@naver.com>
2022-01-23 13:57:23 +0900

    update prisma and Add style

--- todo-list/components/Todo.js
+++ todo-list/components/Todo.js
@@ -1,5 +1,18 @@
+import styled from "@emotion/styled";
+
+function leftTime(completeDate) {
+  const current = new Date();
+}
+
 function Todo({ todo }) {
-  return <div>{todo.todo}</div>;
+  return (
+    <TodoWrapper>
+      <h3>{todo.todo}</h3>
+      <p>{new Date(todo.completeDate)}</p>
+    </TodoWrapper>
+  );
 }
 
 export default Todo;
+
+const TodoWrapper = styled.div``;
--- todo-list/pages/index.js
+++ todo-list/pages/index.js
@@ -2,22 +2,53 @@
 import Head from "next/head";
 import { useSelector } from "react-redux";
 import Todo from "../components/Todo";
+import styled from "@emotion/styled";
 
 export default function Home() {
   const { todos } = useSelector((state) => ({ todos: state.todo }));
 
   return (
-    <div>
+    <MainWrapper>
       <Head>
         <title>Todo list</title>
         <meta name="description" content="practice todo list" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
+      <TodoMain>
+        <Title>chan woo의 Todo list</Title>
         {todos?.map((todo) => (
           <Todo key={todo.id} todo={todo} />
         ))}
-      </main>
-    </div>
+      </TodoMain>
+    </MainWrapper>
   );
 }
+
+const MainWrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: #e2e2e2;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const TodoMain = styled.main`
+  background-color: #fff;
+  width: 600px;
+  height: 700px;
+  border-radius: 20px;
+  padding: 10px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
+
+  @media (max-width: 600px) {
+    width: 100vw;
+    height: 100vh;
+    border-radius: 0;
+  }
+`;
+
+const Title = styled.h1`
+  text-align: center;
+  font-size: 2.5rem;
+`;
