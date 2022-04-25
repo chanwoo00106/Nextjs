e88f5c079a17a0ef9b31bf7ff7db6774d7f9a983
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-04-26 08:12:39 +0900

    Add login

--- netflix/pages/index.tsx
+++ netflix/pages/index.tsx
@@ -1,26 +1,50 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import { useSession, signIn, signOut } from 'next-auth/react'
 
 const Home: NextPage = () => {
+  const { data: session } = useSession()
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center">
+    <div
+      className="flex min-h-screen flex-col items-center justify-center"
+      style={{
+        backgroundImage:
+          'linear-gradient(rgb(0 0 0 / 60%), rgb(0 0 0 / 60%)), url(/hero-bg.jpg)',
+      }}
+    >
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center bg-black px-20 text-center">
+      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+        {session ? (
+          <div className="text-white">
+            Signed in as {session.user?.email}
+            <br />
+            <button onClick={() => signOut()}>logOut</button>
+          </div>
+        ) : (
+          <div className="text-white">
+            Not sined in <br />
+            <button onClick={() => signIn()}>Sign in</button>
+          </div>
+        )}
         <h1 className="max-w-lg text-5xl font-bold text-white">
           Unlimited movies, TV shows, and more.
         </h1>
         <h2 className="mt-4 mb-8 text-2xl font-bold text-white">
           Watch anywhere. Cancel anytime.
         </h2>
-        <p className="text-white">
+        <p className="text-lg text-white">
           Ready to watch? Enter your email to create or restart your membership.
         </p>
         <div className="mt-4 flex">
-          <input className="bg-white p-4" placeholder="Email address" />
+          <input
+            className="min-w-[400px] bg-white p-4"
+            placeholder="Email address"
+          />
           <button className="flex items-center bg-[#e50914] px-4 text-xl text-white ">
             Get Started
             <svg
